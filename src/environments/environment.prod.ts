2759d0e5365c28de7f04f07f2826b6fc9fb54405
Slavik89 <terakrokos@gmail.com>
2021-03-24 19:01:14 +0200

    Base customizing with firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD-NOQk8MxDG4UIBMIGZhheK-3aEVucHUw",
+    authDomain: "frb-school-app.firebaseapp.com",
+    projectId: "frb-school-app",
+    storageBucket: "frb-school-app.appspot.com",
+    messagingSenderId: "1097738440091",
+    appId: "1:1097738440091:web:9db404d3aff0a21e54a53e",
+    measurementId: "G-JMB25BK11Y"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD-NOQk8MxDG4UIBMIGZhheK-3aEVucHUw",
+    authDomain: "frb-school-app.firebaseapp.com",
+    projectId: "frb-school-app",
+    storageBucket: "frb-school-app.appspot.com",
+    messagingSenderId: "1097738440091",
+    appId: "1:1097738440091:web:9db404d3aff0a21e54a53e",
+    measurementId: "G-JMB25BK11Y"
+  }
 };
 
 /*
